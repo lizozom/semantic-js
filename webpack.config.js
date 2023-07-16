006d4f2820922329e04c2639c94f449a9df04567
lizozom <lizka.k@gmail.com>
2023-07-16 19:23:26 +0300

    optimize and clean folder on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/js/index.js',
-    worker: './src/js/worker/index.js',
-    demo: './src/js/demo/index.js',
-  },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  devServer: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin', 
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+module.exports = (_, argv) => {
+  const { mode } = argv;
+  const isDevelopment = mode === 'development';
+
+  const config = {
+    entry: {
+      main: './src/js/index.js',
+      worker: './src/js/worker/index.js',
+      demo: './src/js/demo/index.js',
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-};
+    mode,
+    devtool: isDevelopment ? 'cheap-module-source-map' : 'source-map',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    devServer: {
+      headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin', 
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      })
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    },
+  };
+
+  return config;
+}
